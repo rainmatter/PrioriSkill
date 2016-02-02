c81b1ed523f3eede1b2058b3c9a7f643a38d4d75
Kim S <stewak5@uw.edu>
2016-02-02 15:31:17 -0800

    First commit.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name prioriSkillApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the prioriSkillApp
+ */
+angular.module('prioriSkillApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
